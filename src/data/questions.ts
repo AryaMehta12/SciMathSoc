e02c4facc1abfc38bc5644695b2b136a49c445b0
AryaMehta <73326212+AryaMehta12@users.noreply.github.com>
2025-07-23 13:54:41 +0530

    Update questions.ts

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -10,124 +10,124 @@ export const sampleQuestions: Question[] = [
     subject: 'math',
     difficulty: 'easy'
   },
-  {
-    id: 2,
-    type: 'text-input',
-    question: 'Q2 - Question 2',
-    correctAnswer: '1',
-    subject: 'math',
-    difficulty: 'easy'
-  },
-  {
-    id: 3,
-    type: 'multiple-choice',
-    question: 'Q3 - Question 3',
-    options: ['1', '2', '3', '4'],
-    correctAnswer: '1',
-    subject: 'math',
-    difficulty: 'medium'
-  },
-  {
-    id: 4,
-    type: 'text-input',
-    question: 'Q4 - Question 4',
-    correctAnswer: '1',
-    subject: 'math',
-    difficulty: 'easy'
-  },
-  {
-    id: 5,
-    type: 'multiple-choice',
-    question: 'Q5 - Question 5',
-    options: ['1', '2', '3', '4'],
-    correctAnswer: '1',
-    subject: 'math',
-    difficulty: 'medium'
-  },
-  {
-    id: 6,
-    type: 'multiple-choice',
-    question: 'Q6 - Question 6',
-    options: ['1', '2', '3', '4'],
-    correctAnswer: '1',
-    subject: 'science',
-    difficulty: 'easy'
-  },
-  {
-    id: 7,
-    type: 'text-input',
-    question: 'Q7 - Question 7',
-    correctAnswer: '1',
-    subject: 'science',
-    difficulty: 'easy'
-  },
-  {
-    id: 8,
-    type: 'multiple-choice',
-    question: 'Q8 - Question 8',
-    options: ['1', '2', '3', '4'],
-    correctAnswer: '1',
-    subject: 'science',
-    difficulty: 'hard'
-  },
-  {
-    id: 9,
-    type: 'text-input',
-    question: 'Q9 - Question 9',
-    correctAnswer: '1',
-    subject: 'science',
-    difficulty: 'medium'
-  },
-  {
-    id: 10,
-    type: 'multiple-choice',
-    question: 'Q10 - Question 10',
-    options: ['1', '2', '3', '4'],
-    correctAnswer: '1',
-    subject: 'science',
-    difficulty: 'easy'
-  },
-  {
-    id: 11,
-    type: 'multiple-choice',
-    question: 'Q11 - Question 11',
-    options: ['1', '2', '3', '4'],
-    correctAnswer: '1',
-    subject: 'science',
-    difficulty: 'easy'
-  },
-  {
-    id: 12,
-    type: 'text-input',
-    question: 'Q12 - Question 12',
-    correctAnswer: '1',
-    subject: 'science',
-    difficulty: 'easy'
-  },
-  {
-    id: 13,
-    type: 'multiple-choice',
-    question: 'Q13 - Question 13',
-    options: ['1', '2', '3', '4'],
-    correctAnswer: '1',
-    subject: 'math',
-    difficulty: 'medium'
-  },
-  {
-    id: 14,
-    type: 'text-input',
-    question: 'Q14 - Question 14',
-    correctAnswer: '1',
-    subject: 'science',
-    difficulty: 'easy'
-  },
-  {
-    id: 15,
-    type: 'multiple-choice',
-    question: 'Q15 - Question 15',
-    options: ['1', '2', '3', '4'],
-    correctAnswer: '1',
-    subject: 'math',
-    difficulty: 'easy'
-  }
+  // {
+  //   id: 2,
+  //   type: 'text-input',
+  //   question: 'Q2 - Question 2',
+  //   correctAnswer: '1',
+  //   subject: 'math',
+  //   difficulty: 'easy'
+  // },
+  // {
+  //   id: 3,
+  //   type: 'multiple-choice',
+  //   question: 'Q3 - Question 3',
+  //   options: ['1', '2', '3', '4'],
+  //   correctAnswer: '1',
+  //   subject: 'math',
+  //   difficulty: 'medium'
+  // },
+  // {
+  //   id: 4,
+  //   type: 'text-input',
+  //   question: 'Q4 - Question 4',
+  //   correctAnswer: '1',
+  //   subject: 'math',
+  //   difficulty: 'easy'
+  // },
+  // {
+  //   id: 5,
+  //   type: 'multiple-choice',
+  //   question: 'Q5 - Question 5',
+  //   options: ['1', '2', '3', '4'],
+  //   correctAnswer: '1',
+  //   subject: 'math',
+  //   difficulty: 'medium'
+  // },
+  // {
+  //   id: 6,
+  //   type: 'multiple-choice',
+  //   question: 'Q6 - Question 6',
+  //   options: ['1', '2', '3', '4'],
+  //   correctAnswer: '1',
+  //   subject: 'science',
+  //   difficulty: 'easy'
+  // },
+  // {
+  //   id: 7,
+  //   type: 'text-input',
+  //   question: 'Q7 - Question 7',
+  //   correctAnswer: '1',
+  //   subject: 'science',
+  //   difficulty: 'easy'
+  // },
+  // {
+  //   id: 8,
+  //   type: 'multiple-choice',
+  //   question: 'Q8 - Question 8',
+  //   options: ['1', '2', '3', '4'],
+  //   correctAnswer: '1',
+  //   subject: 'science',
+  //   difficulty: 'hard'
+  // },
+  // {
+  //   id: 9,
+  //   type: 'text-input',
+  //   question: 'Q9 - Question 9',
+  //   correctAnswer: '1',
+  //   subject: 'science',
+  //   difficulty: 'medium'
+  // },
+  // {
+  //   id: 10,
+  //   type: 'multiple-choice',
+  //   question: 'Q10 - Question 10',
+  //   options: ['1', '2', '3', '4'],
+  //   correctAnswer: '1',
+  //   subject: 'science',
+  //   difficulty: 'easy'
+  // },
+  // {
+  //   id: 11,
+  //   type: 'multiple-choice',
+  //   question: 'Q11 - Question 11',
+  //   options: ['1', '2', '3', '4'],
+  //   correctAnswer: '1',
+  //   subject: 'science',
+  //   difficulty: 'easy'
+  // },
+  // {
+  //   id: 12,
+  //   type: 'text-input',
+  //   question: 'Q12 - Question 12',
+  //   correctAnswer: '1',
+  //   subject: 'science',
+  //   difficulty: 'easy'
+  // },
+  // {
+  //   id: 13,
+  //   type: 'multiple-choice',
+  //   question: 'Q13 - Question 13',
+  //   options: ['1', '2', '3', '4'],
+  //   correctAnswer: '1',
+  //   subject: 'math',
+  //   difficulty: 'medium'
+  // },
+  // {
+  //   id: 14,
+  //   type: 'text-input',
+  //   question: 'Q14 - Question 14',
+  //   correctAnswer: '1',
+  //   subject: 'science',
+  //   difficulty: 'easy'
+  // },
+  // {
+  //   id: 15,
+  //   type: 'multiple-choice',
+  //   question: 'Q15 - Question 15',
+  //   options: ['1', '2', '3', '4'],
+  //   correctAnswer: '1',
+  //   subject: 'math',
+  //   difficulty: 'easy'
+  // }
 ];
