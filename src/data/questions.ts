4b3bae0ba7a275941c06d88bffaa4c1ebe995daf
AryaMehta <73326212+AryaMehta12@users.noreply.github.com>
2025-07-24 00:06:17 +0530

    mcq

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -6,7 +6,7 @@ export const sampleQuestions: Question[] = [
     type: 'multiple-choice',
     question: 'Two witches make a nightly visit to an all-night coffee shop. Each arrives at a random time between 0:00 and 1:00. Each one of them stays for exactly 30 minutes. On any one given night, what is the probability that the witches will meet at the coffee shop?',
     options: ['1', '0.5', '0.25', '0.75'],
-    correctAnswer: '1',
+    correctAnswer: '0.75',
     subject: 'math',
     difficulty: 'easy'
   },
